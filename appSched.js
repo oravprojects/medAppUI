5ac17ab40eb3803078d4159eff90e993485240e2
Oren Avni <orenavnimobile@gmail.com>
2021-01-02 22:20:13 +0200

    dynamic appSched

---
+++ appSched.js
@@ -0,0 +1,131 @@
+var test = "test";
+var checkAgain = true;
+var appSched = localStorage.getItem("appSched");
+appSched = JSON.parse(appSched);
+console.log(appSched);
+var appSchedTemp = [];
+var d = new Date();
+var n = d.getTimezoneOffset() / 60;
+if (n < 0) { n = n * -1 };
+var currDate = new Date();
+currDate.setHours(0);
+currDate.setMinutes(0);
+currDate.setSeconds(0);
+currDate.setMilliseconds(0);
+currDate.setHours(currDate.getHours() + n)
+var tomorrow = new Date(currDate);
+tomorrow.setDate(tomorrow.getDate() + 1);
+tomorrow.setHours(0);
+tomorrow.setMinutes(0);
+tomorrow.setSeconds(0);
+tomorrow.setMilliseconds(0);
+tomorrow.setHours(tomorrow.getHours() + n);
+
+for (i = 0; i < appSched.length; i++) {
+    console.log(i + "appSchedArr: " + new Date(appSched[i].date).getTime() + "curr: " + currDate.getTime() +
+        " tomorrow: " + tomorrow.getTime())
+    if (new Date(appSched[i].date).getTime() >= currDate.getTime() &&
+        new Date(appSched[i].date).getTime() < tomorrow.getTime()) {
+        appSchedTemp.push(appSched[i]);
+        console.log(appSchedTemp);
+    }
+}
+
+function loadAppSched() {
+    if (appSchedTemp.length > 0) {
+        var schedContainer = document.getElementById("sched-container");
+        schedContainer.innerHTML = "";
+    }
+    if (appSchedTemp.length > 0) {
+        for (i = 0; i < appSchedTemp.length; i++) {
+            console.log("going: " + i)
+            var firstName = appSchedTemp[i].fName;
+            var lastName = appSchedTemp[i].lName;
+            var startTime = appSchedTemp[i].start;
+            var endTime = appSchedTemp[i].end;
+            var appSchedDate = new Date(appSchedTemp[i].date);
+            var appSchedYear = appSchedDate.getFullYear();
+            var appSchedMonth = appSchedDate.getMonth();
+            var appSchedDay = appSchedDate.getDay();
+            var currDate = new Date();
+            // compensate for GMT offset
+            var d = new Date();
+            var n = d.getTimezoneOffset() / 60;
+            if (n < 0) { n = n * -1 };
+            currDate.setHours(currDate.getHours() + n);
+            var currYear = currDate.getFullYear();
+            var currMonth = currDate.getMonth();
+            var currDay = currDate.getDay();
+            // if (appSchedYear === currYear && appSchedMonth === currMonth && appSchedDay === currDay) {
+                var hour = startTime.substr(0, 2);
+                var endHour = endTime.substr(0, 2);
+                var endMin = endTime.substr(3, 2);
+                var dispHour = hour;
+                var min = startTime.substr(3, 2);
+                if (hour < 12) { startTime += " AM" } else {
+                    dispHour = (hour - 12);
+                    startTime = dispHour + ":" + min + " PM"
+                }
+
+                // append appointment elements
+                var node = document.createElement("div");
+                console.log(currDate + " " + appSchedDate + " currDateMill: " + currDate.getTime() + " appSchedMill: " + (appSchedDate.getTime() + hour * 60 * 60 * 1000 + min * 60 * 1000))
+                console.log("endMill: " + (appSchedDate.getTime() + endHour * 60 * 60 * 1000 + endMin * 60 * 1000));
+                console.log("CHECK: " + checkAgain)
+
+                if (checkAgain) {
+                    if (currDate.getTime() < (appSchedDate.getTime() + hour * 60 * 60 * 1000 + min * 60 * 1000)) {
+                        setTimeout(function () { loadAppSched(); }, ((appSchedDate.getTime() + hour * 60 * 60 * 1000 + min * 60 * 1000) - currDate.getTime()));
+                        if (i === appSchedTemp.length - 1) {
+                            checkAgain = false;
+                            setTimeout(function () { loadAppSched(); }, ((appSchedDate.getTime() + endHour * 60 * 60 * 1000 + endMin * 60 * 1000) - currDate.getTime()));
+                        }
+                    }
+                }
+
+                if (currDate.getTime() >= (appSchedDate.getTime() + hour * 60 * 60 * 1000 + min * 60 * 1000)
+                    && currDate.getTime() < (appSchedDate.getTime() + endHour * 60 * 60 * 1000 + endMin * 60 * 1000)) {
+                    var content = `<div class="dash-appointment" data-toggle="modal" data-target="#exampleModalCenter">
+            <a href="#" class="btn btn-outline-primary w-100 p-2  mb-1 no-ptr current">
+            <ul class="flex-container">
+            <li class="sched-item-icon"><i class="fa fa-user"></i></li>
+            <li class="sched-item-content">${firstName + " " + lastName}</li>
+            <li class="sched-item">${startTime}</li>
+            </ul>
+            </a></div>`
+                } else {
+                    var content = `<div class="dash-appointment" data-toggle="modal" data-target="#exampleModalCenter">
+        <a href="#" class="btn btn-light w-100 p-2 mb-1 no-ptr dashboard">
+        <ul class="flex-container">
+        <li class="sched-item-icon"><i class="fa fa-user"></i></li>
+        <li class="sched-item-content">${firstName + " " + lastName}</li>
+        <li class="sched-item">${startTime}</li>
+        </ul>
+        </a></div>`
+                }
+                node.innerHTML = content;
+                schedContainer.appendChild(node);
+            }
+            // convert time from military time format to 12 hour am/pm format
+        }
+    }
+// }
+// function that allows to add additional window.onload functions
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+addLoadEvent(loadAppSched);
+
+// addLoadEvent(function() {
+//     document.body.style.backgroundColor = '#EFDF95';
+// })
