c26e9e56aa0250b7a1768bf0b0f85375e41a48ce
Oren Avni <orenavnimobile@gmail.com>
2021-04-17 19:06:21 +0300

    search users

--- javascript/chatUsers.js
+++ javascript/chatUsers.js
@@ -1,13 +1,31 @@
 const searchBar = document.querySelector(".users .search input"),
-searchBtn = document.querySelector(".users .search button");
-
-const userName = document.querySelector(".details span");
-userName.textContent = "this is a test"
+searchBtn = document.querySelector(".users .search button"),
+userName = document.querySelector(".details span"),
+status = document.querySelector(".details p"),
+image = document.querySelector(".content img"),
+usersList = document.querySelector(".users .users-list");
 
 searchBtn.onclick = () => {
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
+}
+
+searchBar.onkeyup = ()=>{
+    let searchTerm = searchBar.value;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://127.0.0.1/healthcareProvider/chatSearch.php", true);
+    xhr.withCredentials = true;
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                console.log(data);
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
 }
 
 let xhr = new XMLHttpRequest();
@@ -20,7 +38,9 @@ xhr.onload = () => {
             let data = xhr.response;
             console.log(data);
             if (isJSON(data)) {
-                    userName.textContent = JSON.parse(data).fname + " " + JSON.parse(data).lname
+                    userName.textContent = JSON.parse(data).fname + " " + JSON.parse(data).lname;
+                    status.textContent = JSON.parse(data).status;
+                    image.src = "http://127.0.0.1/healthcareProvider/images/" + JSON.parse(data).image;
             }
             else {
                 console.log("something went wrong . . .")
@@ -29,6 +49,21 @@ xhr.onload = () => {
     }
 }
 xhr.send();
+
+// setInterval(()=>{
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("GET", "http://127.0.0.1/healthcareProvider/chatAvailUsers.php", true);
+//     xhr.withCredentials = true;
+//     xhr.onload = () => {
+//         if (xhr.readyState === XMLHttpRequest.DONE) {
+//             if (xhr.status === 200) {
+//                 let data = xhr.response;
+//                 usersList.innerHTML = data;
+//             }
+//         }
+//     }
+//     xhr.send();
+// }, 500)
 
 function isJSON(str) {
     try {
